b9b79e2f04b7b15540b065c4ebbcc4dfa9330025
agrinevich <grinevich@gmail.com>
2022-05-14 08:36:59 +0300

    Updated backup handling - finished

--- lib/App/Admin/Bkp/Create.pm
+++ lib/App/Admin/Bkp/Create.pm
@@ -23,52 +23,53 @@ sub doit {
     my $bkp_path = $app->config->{bkp}->{path};
 
     my $bkp_name = strftime( '%Y%m%d-%H%M%S', localtime );
+    my $bkp_dir  = $root_dir . $bkp_path . q{/} . $bkp_name;
 
     # 1. templates backup
 
-    my $bkp_dir = $root_dir . $bkp_path . q{/} . $bkp_name . '/tpl';
-    Util::Files::make_path( path => $bkp_dir );
+    my $tplbkp_dir = $bkp_dir . '/tpl';
+    Util::Files::make_path( path => $tplbkp_dir );
 
     my $src_path = $app->config->{templates}->{path_f};
     my $src_dir  = $root_dir . $src_path;
     Util::Files::copy_dir_recursive(
         src_dir => $src_dir,
-        dst_dir => $bkp_dir,
+        dst_dir => $tplbkp_dir,
     );
 
     # 2. database backup
 
-    my $bkp_dir2 = $root_dir . $bkp_path . q{/} . $bkp_name . '/sql';
-    Util::Files::make_path( path => $bkp_dir2 );
+    my $sqlbkp_dir = $bkp_dir . '/sql';
+    Util::Files::make_path( path => $sqlbkp_dir );
 
     # give 'mysql' user permission to write
     my $mode1 = oct '777';
-    chmod $mode1, $bkp_dir2;
+    chmod $mode1, $sqlbkp_dir;
 
     _select_to_file(
         dbh    => $dbh,
-        file   => $bkp_dir2 . '/global_marks.txt',
+        file   => $sqlbkp_dir . '/global_marks.txt',
         table  => 'global_marks',
         fields => 'id, name, value',
     );
 
     _select_to_file(
         dbh    => $dbh,
-        file   => $bkp_dir2 . '/langs.txt',
+        file   => $sqlbkp_dir . '/langs.txt',
         table  => 'langs',
         fields => 'id, name, nick, isocode',
     );
 
     _select_to_file(
         dbh    => $dbh,
-        file   => $bkp_dir2 . '/page_marks.txt',
+        file   => $sqlbkp_dir . '/page_marks.txt',
         table  => 'page_marks',
         fields => 'id, page_id, lang_id, name, value',
     );
 
     _select_to_file(
         dbh   => $dbh,
-        file  => $bkp_dir2 . '/pages.txt',
+        file  => $sqlbkp_dir . '/pages.txt',
         table => 'pages',
         fields =>
             'id, parent_id, priority, hidden, navi_on, changed, mode, child_qty, good_qty, name, nick',
@@ -76,21 +77,21 @@ sub doit {
 
     _select_to_file(
         dbh    => $dbh,
-        file   => $bkp_dir2 . '/plugins.txt',
+        file   => $sqlbkp_dir . '/plugins.txt',
         table  => 'plugins',
         fields => 'id, app, nick',
     );
 
     _select_to_file(
         dbh    => $dbh,
-        file   => $bkp_dir2 . '/notes.txt',
+        file   => $sqlbkp_dir . '/notes.txt',
         table  => 'notes',
         fields => 'id, page_id, hidden, prio, add_dt, price, nick, is_ext, ip',
     );
 
     _select_to_file(
         dbh   => $dbh,
-        file  => $bkp_dir2 . '/notes_versions.txt',
+        file  => $sqlbkp_dir . '/notes_versions.txt',
         table => 'notes_versions',
         fields =>
             'id, note_id, lang_id, name, param_01, param_02, param_03, param_04, param_05, p_title, p_descr, descr',
@@ -98,24 +99,23 @@ sub doit {
 
     _select_to_file(
         dbh    => $dbh,
-        file   => $bkp_dir2 . '/notes_images.txt',
+        file   => $sqlbkp_dir . '/notes_images.txt',
         table  => 'notes_images',
         fields => 'id, note_id, num, path_sm, path_la',
     );
 
+    # reset permission to write
+    my $mode2 = oct '755';
+    chmod $mode2, $sqlbkp_dir;
+
     my $h_zip = Util::Files::create_zip(
-        src_dir => $root_dir . $bkp_path . q{/} . $bkp_name,
+        src_dir => $bkp_dir,
         dst_dir => $root_dir . $bkp_path,
         name    => $bkp_name,
     );
 
-    # reset permission to write
-    my $mode2 = oct '755';
-    chmod $mode2, $bkp_dir2;
-
-    #
-    # TODO: rmdir $root_dir . $bkp_path . q{/} . $bkp_name
-    #
+    Util::Files::empty_dir_recursive( dir => $bkp_dir );
+    rmdir $bkp_dir;
 
     return {
         url => $app->config->{site}->{host} . '/admin/bkp?msg=success',
--- lib/App/Admin/Bkp/Delete.pm
+++ lib/App/Admin/Bkp/Delete.pm
@@ -13,32 +13,13 @@ sub doit {
     my $app       = $args{app};
     my $o_request = $args{o_request};
 
-    my $o_params = $o_request->parameters();
-    my $bkp_name = $o_params->{name} || q{};
+    my $o_params  = $o_request->parameters();
+    my $file_name = $o_params->{name} || q{};
 
     my $root_dir = $app->root_dir;
     my $bkp_path = $app->config->{bkp}->{path};
-    my $bkp_dir  = $root_dir . $bkp_path . q{/} . $bkp_name;
 
-    my $dst_dir = $bkp_dir . '/tpl';
-    if ( -d $dst_dir ) {
-        Util::Files::empty_dir_recursive(
-            dir => $dst_dir,
-        );
-        rmdir $dst_dir;
-    }
-
-    my $dst_dir2 = $bkp_dir . '/sql';
-    if ( -d $dst_dir2 ) {
-        Util::Files::empty_dir_recursive(
-            dir => $dst_dir2,
-        );
-        rmdir $dst_dir2;
-    }
-
-    rmdir $bkp_dir;
-
-    my $zip = $root_dir . $bkp_path . q{/} . $bkp_name . '.zip';
+    my $zip = $root_dir . $bkp_path . q{/} . $file_name;
     unlink $zip;
 
     return {
